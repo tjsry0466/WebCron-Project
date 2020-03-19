7d59d00a35673b2f24f9acce74448dacf7770a88
seonkyojeong <tjs0466@naver.com>
2020-03-20 00:51:03 +0900

    commit

--- cron/models.py
+++ cron/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Schedule(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    fieldir = models.CharField(max_length=100)
+    second = models.CharField(max_length=2)
+    minute =  models.CharField(max_length=2)
+    day = models.CharField(max_length=2)
+    week = models.CharField(max_length=2)
+    month = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
