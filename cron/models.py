7d150f3547a8c892c7430c84a176cbe27fa4dd61
seonkyojeong <tjs0466@naver.com>
2020-03-20 01:11:30 +0900

    add logdir

--- cron/models.py
+++ cron/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class Schedule(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     fieldir = models.CharField(max_length=100)
+    logdir = models.CharField(max_length=100)
     second = models.CharField(max_length=2)
     minute =  models.CharField(max_length=2)
     day = models.CharField(max_length=2)
