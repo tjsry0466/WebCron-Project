d7cba7721c2ee30573aa45bef8432868ce28c655
seonkyojeong <tjs0466@naver.com>
2020-03-20 01:13:44 +0900

    change model

--- cron/models.py
+++ cron/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 class Schedule(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
     fieldir = models.CharField(max_length=100)
     logdir = models.CharField(max_length=100)
     second = models.CharField(max_length=2)
