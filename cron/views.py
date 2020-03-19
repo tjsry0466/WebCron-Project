de08c1c05eebc238b1ae3b9a0487c262583b855b
seonkyojeong <tjs0466@naver.com>
2020-03-20 01:30:24 +0900

    commit

--- cron/views.py
+++ cron/views.py
@@ -1,7 +1,14 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
+from .models import Schedule
+
 
 def test(request):
-    a = os.popen('crontab -l').read()
-    return HttpResponse(a)
+    # a = os.popen('crontab -l').read()
+    obj = Schedule.objects.all().first()
+    f = open(f'{obj.user.username}_{obj.name}.py', 'w')
+    print(obj.script)
+    f.write(obj.script)
+    f.close()
+    return HttpResponse("hello")
